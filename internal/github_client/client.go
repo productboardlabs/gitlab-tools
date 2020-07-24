3e5d0e9a6dd2465efcb3abbcf73afcd96c41f233
Fallion <simon@fallion.net>
2020-07-24 15:26:17 +0200

    chore!: replace githubv4 with github.com/google/go-github
    BREAKING CHANGE: The GraphQL API of Github doesn't support all the features that the old API had. Therefore we need to switch to the old ones.
    
    Particularly the Status check API is needed.

--- internal/github_client/client.go
+++ internal/github_client/client.go
@@ -3,13 +3,13 @@ package githubclient
 import (
 	"context"
 
-	"github.com/shurcooL/githubv4"
+	"github.com/google/go-github/v32/github"
 	"golang.org/x/oauth2"
 )
 
 // Client contains the set up github client and allowed queries to communicate with Github.
 type Client struct {
-	ghClient *githubv4.Client
+	ghClient *github.Client
 }
 
 // New bootstraps the Client struct
@@ -19,7 +19,7 @@ func New(token string) *Client {
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
 
-	client := githubv4.NewClient(httpClient)
+	client := github.NewClient(httpClient)
 
 	return &Client{
 		ghClient: client,
